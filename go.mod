75d9cb81ecd80a94dc72feae74405932c792e201
Milos Pejanovic <pejan123@gmail.com>
2024-06-10 12:14:09 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/runz0rd/jerkins
+
+go 1.22.3
+
+require (
+	github.com/bitfield/script v0.22.1
+	github.com/bndr/gojenkins v1.1.0
+	github.com/joho/godotenv v1.5.1
+	github.com/peterbourgon/ff/v3 v3.4.0
+	github.com/pkg/errors v0.9.1
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/itchyny/gojq v0.12.13 // indirect
+	github.com/itchyny/timefmt-go v0.1.5 // indirect
+	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
+	mvdan.cc/sh/v3 v3.7.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log/slog"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/bitfield/script"
+	"github.com/bndr/gojenkins"
+	"github.com/joho/godotenv"
+	"github.com/peterbourgon/ff/v3"
+	"github.com/pkg/errors"
+	"gopkg.in/yaml.v3"
+)
+
+func main() {
+	fs := flag.NewFlagSet("search", flag.ContinueOnError)
+	var (
+		flagJenkinsBase = fs.String("base", "https://jenkins.general.mzg.bestbytes.net", "jenkins server base")
+		flagUsername    = fs.String("jenkins-user", "", "jenkins username")
+		flagPassword    = fs.String("jenkins-pass", "", "jenkins password")
+		flagJob         = fs.String("job", "websop-pipeline-kubernetes-single", "jenkins job")
+		flagJobParams   = fs.String("params", "jerkins.yaml", "yaml file with jenkins job params")
+	)
+	err := godotenv.Load()
+	if err != nil {
+		slog.Warn(err.Error())
+	}
+	if err := ff.Parse(fs, os.Args[1:], ff.WithEnvVars()); err != nil {
+		panic(err)
+	}
+
+	slog.SetLogLoggerLevel(slog.LevelDebug)
+	ctx := context.Background()
+	jc := gojenkins.CreateJenkins(nil, *flagJenkinsBase, *flagUsername, *flagPassword)
+	if _, err := jc.Init(ctx); err != nil {
+		panic(err)
+	}
+	// load job params
+	f, err := os.Open(*flagJobParams)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	params := make(jobParams)
+	if err := yaml.NewDecoder(f).Decode(&params); err != nil {
+		panic(err)
+	}
+	if err := params.fillInValues(); err != nil {
+		panic(err)
+	}
+	uncommitedNum, err := getUncommitedChanges()
+	if err != nil {
+		panic(err)
+	}
+	if uncommitedNum > 0 {
+		branch, _ := getCurrentBranch()
+		slog.Warn("you have uncommited changes on your current branch", "branch", branch, "len", uncommitedNum)
+	}
+	queueId, err := jc.BuildJob(ctx, *flagJob, params)
+	if err != nil {
+		panic(err)
+	}
+	build, err := jc.GetBuildFromQueueID(ctx, queueId)
+	if err != nil {
+		panic(err)
+	}
+	slog.Debug("build started", "job", *flagJob)
+	// Wait for build to finish
+	for build.IsRunning(ctx) {
+		slog.Debug("building")
+		time.Sleep(5000 * time.Millisecond)
+		build.Poll(ctx)
+	}
+	slog.Info("build done", "result", build.GetResult())
+	if build.GetResult() == "FAILURE" {
+		slog.Info("logs", "link", fmt.Sprintf("%v/job/%v/%v/console", *flagJenkinsBase, *flagJob, build.GetBuildNumber()))
+	}
+}
+
+func getCurrentBranch() (string, error) {
+	out, err := script.Exec("git rev-parse --abbrev-ref HEAD").String()
+	if err != nil {
+		return "", errors.WithMessage(err, out)
+	}
+	return strings.Trim(out, "\n\t "), nil
+}
+
+func getUncommitedChanges() (int, error) {
+	return script.Exec("git status --porcelain=v1").CountLines()
+}
+
+func getShortHash(branch string) (string, error) {
+	out, err := script.Exec("git rev-parse --short --verify " + branch).String()
+	if err != nil {
+		return "", errors.WithMessage(err, out)
+	}
+	return out, nil
+}
+
+type jobParams map[string]string
+
+func (jp jobParams) fillInValues() error {
+	branch, err := getCurrentBranch()
+	if err != nil {
+		return err
+	}
+	for k, v := range jp {
+		if v != "" {
+			continue
+		}
+		switch strings.ToLower(k) {
+		case "branch":
+			jp[k] = branch
+		case "tag":
+			tag, err := getShortHash(branch)
+			if err != nil {
+				return err
+			}
+			jp[k] = tag
+		}
+	}
+	return nil
+}
